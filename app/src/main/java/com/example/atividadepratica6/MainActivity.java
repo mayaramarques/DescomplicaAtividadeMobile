251835fdbc94a1eb9cbd13192d8b3decc87ba030
Mayara C X Marques <mayaracxmarques@gmail.com>
2022-10-30 19:57:17 -0300

    AtividadePratica6 - Descomplica

---
+++ app/src/main/java/com/example/atividadepratica6/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.atividadepratica6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void callSecondActivity(View view) {
+        Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+        i.putExtra("Value1", "Aula5");
+        i.putExtra("Value2", "Descomplica");
+        startActivity(i);
+    }
+}
